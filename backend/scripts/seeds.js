753f7314a99bb6c58c44c270735172031ffe8d32
Mayank-2112 <113326202+Mayank-2112@users.noreply.github.com>
2024-04-09 15:44:04 +0000

    changes made

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,4 +1,7 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+const mongoose = require("mongoose");
+const connection = process.env.MONGODB_URI;
+mongoose.connect(connection);
 
 const User = mongoose.model("User");
 const Item = mongoose.model("Item");
